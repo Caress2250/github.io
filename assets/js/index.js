2f0ed9962a9f902296c44dc9d83a4a5187d1ce34
Caress2250 <129928575+Caress2250@users.noreply.github.com>
2023-05-25 11:15:58 +0900

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,71 +1,56 @@
+// URLパラメータからページ番号を取得します
 const urlParams = new URLSearchParams(window.location.search);
-let page = Number(urlParams.get('page')) || 1; 
+let page = Number(urlParams.get('page')) || 1; // URLのパラメータからpageを取得し、なければデフォルトは1 (最新の記事)
 
-let apiIndex = "https://2mmx7gzu7i.microcms.io/api/v1/blogs"; 
+let apiIndex = "https://2mmx7gzu7i.microcms.io/api/v1/blogs"; // APIのエンドポイント
 
+// ブログデータを取得する関数
 const getBlogData = () => {
-  let offset = (page - 1) * 5; 
+  let offset = (page - 1) * 5; // 5記事ずつ表示するため、offsetは (ページ数 - 1) * 5
+  // APIからデータを取得します
   fetch(`${apiIndex}?limit=5&offset=${offset}`, {
     headers: {
-      "X-API-KEY": "PEfp0AHyALckYsrJavaQSj0l1KOg6LhIUOww" 
+      "X-API-KEY": "PEfp0AHyALckYsrJavaQSj0l1KOg6LhIUOww" // APIキー
     }
   })
   .then(res => res.json())
   .then(json => {
-    let totalArticles = json.totalCount; 
-
+    let totalArticles = json.totalCount; // 総記事数を取得
     for (let i = 0; i < 5; i++) {
+      // 取得したデータを元に表示を更新します
       if (json.contents.length > i) {
         let latestId = json.contents[i].id;
-        fetchSingleBlogData(latestId, i); 
+        fetchSingleBlogData(latestId, i); // 各記事データを取得
       } else {
-        document.getElementById(`article${i}`).style.display = "none"; 
+        document.getElementById(`article${i}`).style.display = "none"; // データがなければ該当の記事を非表示にします
       }
-    }
-
-    let nextPage = document.querySelector('.button.large.next');
-    if (json.contents.length === 5 && (page + 1) * 5 < totalArticles) {
-      nextPage.href = `?page=${page + 1}`; 
-      nextPage.style.display = "";
-      nextPage.addEventListener('click', function(event) {
-        event.preventDefault();
-        window.location.href = `?page=${page + 1}`; 
-      });
-    } else {
-      nextPage.style.display = "none"; 
-    }
-
-    if (page > 1 && page * 5 <= totalArticles) {
-      let prevPage = document.querySelector('.button.large.previous');
-      prevPage.href = `?page=${page - 1}`;
-      prevPage.style.display = ""; 
-      prevPage.addEventListener('click', function(event) {
-        event.preventDefault();
-        window.location.href = `?page=${page - 1}`;
-      });
-    } else {
-      let prevPage = document.querySelector('.button.large.previous');
-      prevPage.style.display = "none";
     }
   })
 }
 
+// 個々のブログデータを取得する関数
 const fetchSingleBlogData = (latestId, index) => {
-  let apiUrl = `${apiIndex}/${latestId}`; 
+  let apiUrl = `${apiIndex}/${latestId}`; // 最新のIDを使ってAPIのURLを組み立てる
+  // 個々の記事データをAPIから取得します
   fetch(apiUrl, {
     headers: {
-      "X-API-KEY": "PEfp0AHyALckYsrJavaQSj0l1KOg6LhIUOww"
+      "X-API-KEY": "PEfp0AHyALckYsrJavaQSj0l1KOg6LhIUOww" // APIキー
     }
   })
   .then(res => res.json())
   .then(json => {
-    document.getElementById(`date${index}`).innerHTML = moment(json.publishedAt).format('YYYY年MM月DD日');
-    document.getElementById(`title${index}`).innerHTML = json.title;
-    document.getElementById(`content${index}`).innerHTML = json.content;
-    document.getElementById(`eyecatch${index}`).src = json.eyecatch.url;
+    // 取得したデータを使って表示を更新します
+    document.getElementById(`date${index}`).innerHTML = moment(json.publishedAt).format('YYYY年MM月DD日'); // 日付を表示
+    document.getElementById(`title${index}`).innerHTML = json.title; // タイトルを表示
+    document.getElementById(`content${index}`).innerHTML = json.content; // コンテンツを表示
+    let imgElement = document.getElementById(`eyecatch${index}`);
+    imgElement.src = json.eyecatch.url; // アイキャッチ画像を表示
+    imgElement.setAttribute("loading", "lazy"); // 遅延読み込み属性を追加
+    
+    // Add URL parameter to "Read more" button
+    document.getElementById(`button${index}`).href = `page.html?id=${latestId}`;
   })
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-  getBlogData(); 
-});
+// APIから指定されたページのブログ記事を取得します
+getBlogData();
