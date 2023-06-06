f5313a392fc4da4a95d049e4ed21b1b27cdcdeee
Caress2250 <loveawanakulake0522@gmail.com>
2023-06-06 19:57:30 +0900

    Update page.js

--- assets/js/page.js
+++ assets/js/page.js
@@ -1,7 +1,9 @@
 const urlParams = new URLSearchParams(window.location.search);
 let id = urlParams.get('id'); // URLパラメータからIDを取得
+
 let apiIndex = "https://2mmx7gzu7i.microcms.io/api/v1/blogs"; // APIのエンドポイント
 let apiUrl = `${apiIndex}/${id}`; // URLパラメータのIDを使ってAPIのURLを組み立てる
+
 fetch(apiUrl, {
   headers: {
     "X-API-KEY": "PEfp0AHyALckYsrJavaQSj0l1KOg6LhIUOww" // APIキー
@@ -18,20 +20,29 @@ fetch(apiUrl, {
 .catch(err => {
     console.error('APIリクエストでエラーが発生しました：', err);
   });
+
+
+
+  
   // コメント機能を実装する
 window.onload = function() {
   // コメント領域を初期化します
   let commentoDiv = document.getElementById('commento');
   commentoDiv.innerHTML = '';
+
   // Commentoのスクリプトタグを作成します
   let script = document.createElement('script');
   script.src = 'https://cdn.commento.io/js/commento.js';
   script.defer = true;
+
   // 記事ごとに一意の識別子を持つようにdata-page-idを設定します
   script.dataset.pageId = id;  // idはURLパラメータから取得しています
+
   // Commentoのスクリプトタグをコメント領域に追加します
   commentoDiv.appendChild(script);
 }
-document.getElementById('back-button').addEventListener('click', function() {
-  window.history.back();
-});
+
+
+// document.getElementById('back-button').addEventListener('click', function() {
+//   window.history.back();
+// });
