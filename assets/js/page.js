226640435677e23c13066b300cbd850f14e54945
Caress2250 <loveawanakulake0522@gmail.com>
2023-06-07 13:02:37 +0900

    Update page.js

--- assets/js/page.js
+++ assets/js/page.js
@@ -8,8 +8,27 @@ window.onload = function() {
   let script = document.createElement('script');
   script.src = 'https://cdn.commento.io/js/commento.js';
   script.defer = true;
-  script.dataset.pageId = id;
+  // script.dataset.pageId = id;
   commentoDiv.appendChild(script);
+
+  // URLパラメータからページIDを取得する
+  const urlParams = new URLSearchParams(window.location.search);
+  const pageId = urlParams.get('id');  // 'id'はURLパラメータの名前です。適切なものに変更してください。
+
+  // APIからページデータを取得する
+  fetch('https://your-api-endpoint.com/page/' + pageId)
+    .then(response => response.json())
+    .then(data => {
+      // 取得したデータをページに反映する
+      document.getElementById('title0').innerText = data.title;
+      document.getElementById('content0').innerText = data.content;
+      document.getElementById('eyecatch0').src = data.image;
+      document.getElementById('date0').innerText = moment(data.date).format('LL');  // moment.jsを使って日付を整形
+    })
+    .catch(error => {
+      // エラーハンドリング
+      console.error('Error:', error);
+    });
 
   // コメントが投稿されたときにアクションフラグを更新するイベントリスナーを追加（例として）
   commentoDiv.addEventListener('commentPosted', function() {
@@ -18,10 +37,10 @@ window.onload = function() {
   });
 
   // ログインが行われたときにアクションフラグを更新するイベントリスナーを追加（例として）
-  document.getElementById('login-button').addEventListener('click', function() {
-    actionTaken = true;
-    updateBackButton();
-  });
+  // document.getElementById('login-button').addEventListener('click', function() {
+  //  actionTaken = true;
+  //  updateBackButton();
+  // });
 }
 
 function updateBackButton() {
